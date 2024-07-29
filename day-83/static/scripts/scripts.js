77ac01af6adffe12d5994fa1326d9282411494ce
Berkan Sözer <berkansozer@outlook.com>
2024-07-29 17:07:18 +0300

    Add day 83 -> Navigation bar is done, hero section added

---
+++ day-83/static/scripts/scripts.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const dropdownButton = document.getElementById('dropdownNavbarLink');
+//     const dropdownMenu = document.getElementById('dropdownNavbar');
+
+//     // Show menu on mouseenter
+//     dropdownButton.addEventListener('mouseenter', function () {
+//         dropdownMenu.classList.remove('hide');
+//         dropdownMenu.classList.add('show');
+//     });
+//     // Hide menu on mouseleave
+//     dropdownMenu.addEventListener('mouseleave', function () {
+//         dropdownMenu.classList.remove('show');
+//         dropdownMenu.classList.add('hide');
+//     });
+// });
